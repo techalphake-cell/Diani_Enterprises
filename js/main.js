05a29cd7c721a1be77662e4509cef94b5fb7fa9d
Kingi <kingisalim791@gmail.com>
2026-07-06 21:24:21 +0300

    feat: add mobilr responsoveness and hamburger menu

---
+++ js/main.js
@@ -0,0 +1,10 @@
+document.addEventListener('DOMContentLoaded', () => {
+  const navToggle = document.querySelector('.nav-toggle');
+  const navLinks = document.querySelector('.nav-links');
+
+  navToggle.addEventListener('click', () => {
+    navLinks.classList.toggle('nav-open');
+    navToggle.classList.toggle('active');
+
+  });
+});
